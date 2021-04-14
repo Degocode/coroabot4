0db83d1f123ce32d9771a9808bbd3ce593c3cc62
degocode <vozvideo.silva@gmail.com>
2021-04-14 07:47:02 -0500

    First commit

---
+++ src/help.js
@@ -0,0 +1,164 @@
+const help = (prefix) => {
+	return `
+*⚔️Não tirem a coroa👑🇧🇷*
+*🔞Lendaria coroa👑🍆*
+ <══════════════════════>
+*👉.menu👌*
+〘 INFORMAÇÕES 〙
+
+◣_◢ (Noah 2.5) ◣_◢
+👑 𝐃𝐎𝐍𝐎: Andre
+👑 *wa.me/+5568999068797*
+👑 𝐒𝐓𝐀𝐓𝐔𝐒: ON
+
+╔════•ೋೋ•════╗ 
+   *Menu🏰*
+👑 *${prefix}figu*
+👑 *${prefix}toimg*
+👑 *${prefix}pinterest(pesquisar img)*
+👑 *${prefix}leens [na legenda]*
+👑 *${prefix}wait [marca img anime]*
+👑 *${prefix}ytmp4*
+👑 *${prefix}tomp3*
+👑 *${prefix}play*
+╚════•ೋೋ•════╝
+
+╔════•ೋೋ•════╗ 
+   *Diversão🎉*
+👑 *${prefix}meme*
+👑 *${prefix}darkjokes (memes)*
+👑 *${prefix}mandememe*
+👑 *${prefix}leens [marca img que tem palavra]*
+👑 *${prefix}roleta [@]*
+👑 *${prefix}casal [@]*
+👑 *${prefix}punheteiros [@]*
+👑 *${prefix}gay [@]*
+👑 *${prefix}gado [@]*
+👑 *${prefix}buc [@]*
+👑 *${prefix}lgbt [@]*
+👑 *${prefix}passatempo*
+👑 *${prefix}nulis(texto) [@]*
+👑 *${prefix}assttp(texto) [@]*
+👑 *${prefix}text3d(texto) [@]*
+👑 *${prefix}testime*
+👑 *${prefix}timer*
+👑 *${prefix}alerta*
+👑 *${prefix}next*
+╚════•ೋೋ•════╝
+
+╔════•ೋೋ•════╗ 
+   *Imagens😊*
+👑 *${prefix}modoanime [on/off]*
+👑 *${prefix}naruto [aleatórias]*
+👑 *${prefix}hinata [aleatórias]*
+👑 *${prefix}boruto [aleatórias]*
+👑 *${prefix}minato [aleatórias]*
+👑 *${prefix}sakura [aleatórias]*
+👑 *${prefix}sasuke [aleatórias]*
+👑 *${prefix}rize [aleatórias]* 
+👑 *${prefix}loli*
+👑 *${prefix}loli1*
+👑 *${prefix}hentai*
+👑 *${prefix}bomdia*
+👑 *${prefix}boatarde* 
+👑 *${prefix}boanoite*
+╚════•ೋೋ•════╝
+
+╔════•ೋೋ•════╗ 
+   *Imagens🔞*
+👑 *${prefix}modonsfw [on/off]*
+👑 *${prefix}nsfwloli [apenas no pv]*
+👑 *${prefix}trap*
+👑 *${prefix}porno*
+👑 *${prefix}belle*
+╚════•ೋೋ•════╝
+
+╔════•ೋೋ•════╗
+   *Level do Grupo*
+👑 *${prefix}leveling [on/off]*
+👑 *${prefix}level*
+╚════•ೋೋ•════╝
+
+╔════•ೋೋ•════╗ 
+    *Informação do Grupo*
+👑 *${prefix}bemvindo [1/0]*
+👑 *${prefix}grupoinfo(Vejas as Regras)*
+👑 *${prefix}dono*
+👑 *${prefix}canal*
+👑 *${prefix}termux*
+╚════•ೋೋ•════╝
+
+╔════•ೋೋ•════╗ 
+   *Para os Adms*
+👑 *${prefix}marcar*
+👑 *${prefix}marcar2*
+👑 *${prefix}marcar3*
+👑 *${prefix}banir*
+👑 *${prefix}add [@]*
+👑 *${prefix}banir [@]*
+👑 *${prefix}promover [@]*
+👑 *${prefix}rebaixar*
+👑 *${prefix}admins*
+👑 *${prefix}linkgp*
+👑 *${prefix}nomegp*
+👑 *${prefix}bloqueados*
+👑 *${prefix}bloquear [@]*
+👑 *${prefix}desbloquear [@]*
+👑 *${prefix}limpar*
+👑 *${prefix}clonar [@]*
+👑 *${prefix}menu1*
+👑 *${prefix}ping [ver velocidade do bot]*
+👑 *${prefix}wame (criar link do seu whatsapp*
+👑 *${prefix}desligar (so para o dono)*
+👑 *${prefix}setfoto (alterar foto do grupo)*
+👑 *${prefix}setnome (alterar nome do grupo)*
+👑 *${prefix}setdesc (Mudar a descrição do grupo)*
+╚════•ೋೋ•════╝
+
+╔════•ೋೋ•════╗ 
+   *Melhorar Grupo*
+👑 *${prefix}antiracismo [on/off]*
+👑 *${prefix}antilink [1/0]*
+╚════•ೋೋ•════╝
+
+╔════•ೋೋ•════╗ 
+    *Comandos de Voz🤖*
+👑 *${prefix}ola*
+👑 *${prefix}bv*
+👑 *${prefix}tchau*
+👑 *${prefix}bem*
+👑 *${prefix}a*
+👑 *${prefix}fdp*
+👑 *${prefix}onich*
+👑 *${prefix}beat1*
+👑 *${prefix}glub*
+╚════•ೋೋ•════╝
+
+╔════•ೋೋ•════╗ 
+   *MSG BOT🤖*
+🤖NOTA »
+Mandar a msg sem o prefixo
+👑 *bah*
+👑 *oii*
+👑 *bv*
+👑 *canta ai bot*
+👑 *grita*
+👑 *gemidao*
+👑 *musica*
+╚════•ೋೋ•════╝
+
+╔════•ೋೋ•════╗ 
+|￣￣￣￣￣￣￣￣￣|
+|*Noah Bot 2.0*   |
+|*NOME: ANDRÉ*     |
+|*WPP: wa.me/+5568999068797*|
+|＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿|
+(\__/) ||
+(•ㅅ•) ||
+/ 　 づ
+
+╚═〘 André noah 〙`
+}
+
+exports.help = help
+
